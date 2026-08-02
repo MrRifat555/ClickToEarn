a91d4b8ec2ffc049e1579651a9ab28ab074e6db1
MrRifat555 <alexrifatmahmud@gmail.com>
2026-08-02 14:45:49 +0600

    Initialize database automatically

---
+++ database/database.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+DB = "users.db"
+
+def init_db():
+    conn = sqlite3.connect(DB)
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY,
+        username TEXT,
+        first_name TEXT,
+        balance REAL DEFAULT 0,
+        referrals INTEGER DEFAULT 0
+    )
+    """)
+
+    conn.commit()
+    conn.close()
